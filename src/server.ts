283c3d3eca411c958019df024d989353bb3af850
Visions-Nicolas <nicolas@visionspol.eu>
2023-11-24 17:23:37 +0100

    fix: static file path change if not development env

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import { loadRoutes } from "./routes";
 import { loadMongoose } from "./config/database";
 import path from "path";
+import * as process from "process";
 
 export const startServer = (testPort?: number) => {
   loadMongoose();
@@ -15,11 +16,20 @@ export const startServer = (testPort?: number) => {
 
   loadRoutes(app);
 
+  if(process.env.NODE_ENV !== "development"){
+    app.use(
+      "/static/references",
+      express.static(path.join(__dirname, "..", "..", "static"))
+    );
+  } else {
+    app.use(
+      "/static/references",
+      express.static(path.join(__dirname, "..", "static"))
+    );
+  }
+
   // use of static files for json-ld
-  app.use(
-    "/static/references",
-    express.static(path.join(__dirname, "..", "..", "static"))
-  );
+
 
   // Start the server
   const server = app.listen(port, () => {
